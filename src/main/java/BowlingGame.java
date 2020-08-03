6e98032aae27762aabbcb5663378bdc2182e73dc
Lwolwo <zuihouyiye0704@126.com>
2020-08-04 00:09:12 +0800

    feat: case2:when frames spare but not strike

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,8 +1,27 @@
 public class BowlingGame {
     public int caclScores(int[] scores) {
         int tot = 0;
+        int line = 1;
+        boolean nextFrame = false;
+        int frameTot = 0;
         for (int i = 0; i < scores.length; i++) {
-            tot += scores[i];
+            frameTot += scores[i];
+
+            if (nextFrame) {
+                nextFrame = false;
+                // 补中情况
+                if (frameTot == 10) {
+                    tot += 10 + scores[i];
+                } else {
+                    tot += scores[i] + scores[i - 1];
+                }
+
+                frameTot = 0;
+                line++;
+            } else {
+                nextFrame = true;
+            }
+
         }
         System.out.println(tot);
         return tot;
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -5,8 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingGameTest {
     @Test
     void should_get_scores_when_every_frame_no_strike_and_spare() {
-        int[] scores = { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4 };
+        int[] scores = { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4 };
         int result = new BowlingGame().caclScores(scores);
-        assertEquals(82, result);
+        assertEquals(50, result);
+    }
+
+    @Test
+    void should_get_scores_when_not_last_frame_spare_and_no_strike() {
+        int[] scores = { 1, 9, 8, 2, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4 };
+        int result = new BowlingGame().caclScores(scores);
+        assertEquals(71, result);
     }
 }
